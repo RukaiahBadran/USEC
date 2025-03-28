b7920877c627c0776490dcd0264af14629de7497
RukaiahBadran <rukaiahbadran@gmail.com>
2025-03-28 01:56:10 +0000

    usec

---
+++ analysis.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import chi2_contingency
+
+romwe = pd.read_excel(r'C:\Users\runab\Documents\GitHub\USEC\ROMWE.xlsx')
+
+# cleaning
+clean = romwe.dropna(subset=['USE', 'UNDERSTANING', 'RATING', 'MALICIOUS'], how='all')
+clean.head()
+
+# Plot
+questions = ['USE', 'UNDERSTANING', 'RATING', 'MALICIOUS']
+
+for question in ['USE', 'UNDERSTANING', 'RATING', 'MALICIOUS']:
+    plt.figure(figsize=(8, 5))
+    sns.countplot(data=clean, x=question)
+    plt.title(f'Response Distribution for: {question}')
+    plt.xticks(rotation=45)
+    plt.show()
+
+# Chi analysis
+clean['MaliciousBinary'] = clean['MALICIOUS'].replace({'No': 0, 'Yes': 1})
+
+clean['EaseOfUse'] = clean['USE'].factorize()[0]
+clean['EaseOfUnderstand'] = clean['UNDERSTANING'].factorize()[0]
+clean['AppRating'] = clean['RATING'].factorize()[0]
+
+
+hypotheses = [
+    ('EaseOfUnderstand', 'MaliciousBinary'),
+    ('EaseOfUse', 'MaliciousBinary'),
+    ('AppRating', 'MaliciousBinary')
+]
+
+for hypo in hypotheses:
+    contingency = pd.crosstab(clean[hypo[0]], clean[hypo[1]])
+    chi2, p, dof, expected = chi2_contingency(contingency)
+    print(f'Hypothesis: {hypo[0]} vs {hypo[1]}')
+    print(f'Chi-square value: {chi2}, p-value: {p}')
+    print(f'Degrees of freedom: {dof}')
+    print(f'Expected frequencies: \n{expected}\n')
+    print('-' * 50)
